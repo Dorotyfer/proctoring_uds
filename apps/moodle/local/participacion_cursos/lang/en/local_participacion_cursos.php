26ef740f5732655ed56604f3cf535d7b5eca5747
Dorotyfer <106775888+Dorotyfer@users.noreply.github.com>
2026-08-21 09:33:25 -0300

    fix: localize participation report and link review panel

---
+++ apps/moodle/local/participacion_cursos/lang/en/local_participacion_cursos.php
@@ -0,0 +1,13 @@
+<?php
+
+$string['pluginname'] = 'Participación de cursos';
+$string['tab_name'] = 'Estadísticas del curso';
+$string['page_heading'] = 'Análisis de interacción';
+$string['nodata'] = 'Aún no se registran actividades completadas, cuestionarios, entregas de tareas o participaciones en foros en este curso.';
+$string['chart_title'] = 'Distribución de participación del curso';
+$string['series_participation'] = 'Participación';
+$string['metric_activities'] = 'Actividades completadas';
+$string['metric_quizzes'] = 'Intentos de cuestionarios';
+$string['metric_assignments'] = 'Tareas entregadas';
+$string['metric_forums'] = 'Mensajes en foros';
+$string['open_panel'] = 'Abrir panel de proctoring';
