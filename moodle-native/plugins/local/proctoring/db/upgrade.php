b2c7cbcccdce4b63fdc21864df8f2abf9392c4d0
Dorotyfer <106775888+Dorotyfer@users.noreply.github.com>
2026-09-01 12:12:28 -0300

    fix: migrate native proctoring schema on Moodle

--- moodle-native/plugins/local/proctoring/db/upgrade.php
+++ moodle-native/plugins/local/proctoring/db/upgrade.php
@@ -3,5 +3,32 @@
 defined('MOODLE_INTERNAL') || die();
 
 function xmldb_local_proctoring_upgrade(int $oldversion): bool {
+    global $DB;
+
+    if ($oldversion < 2026090101) {
+        $dbman = $DB->get_manager();
+        $tables = [
+            'local_proctoring_policy',
+            'local_proctoring_session',
+            'local_proctoring_event',
+            'local_proctoring_alert',
+            'local_proctoring_evidence',
+            'local_proctoring_evaudit',
+            'local_proctoring_biometric',
+            'local_proctoring_biover',
+            'local_proctoring_biocheck',
+            'local_proctoring_envsignal',
+            'local_proctoring_riskscore',
+        ];
+
+        foreach ($tables as $table) {
+            if (!$dbman->table_exists(new xmldb_table($table))) {
+                $dbman->install_one_table_from_xmldb_file(__DIR__ . '/install.xml', $table);
+            }
+        }
+
+        upgrade_plugin_savepoint(true, 2026090101, 'local', 'proctoring');
+    }
+
     return true;
 }
--- moodle-native/plugins/local/proctoring/version.php
+++ moodle-native/plugins/local/proctoring/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_proctoring';
-$plugin->version = 2026090100;
+$plugin->version = 2026090101;
 $plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1.0-native';
